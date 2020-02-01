89f7488775a5b2cda8c86c527540ffbdf9966b88
Ekanshi Agrawal <ekanshi258@gmail.com>
2020-02-02 02:00:21 +0530

    Mallu-Bhawan . csv

--- src/hosttrack.py
+++ src/hosttrack.py
@@ -21,6 +21,7 @@ for host, status in hosts_list:
     print(host + ' ' + status)
 
 fp = open("hosttrack.csv","a")
+#fp = open("hostmm.csv","a")        #for Malaviya
 w = csv.writer(fp)
 row = [time, len(hosts_list)]
 w.writerow(row)
