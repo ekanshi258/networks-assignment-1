667f33d560ce2661984576b7ade7ac86ab94dc27
Ekanshi Agrawal <ekanshi258@gmail.com>
2020-02-04 19:05:08 +0530

    mallu 4th feb

--- src/hosttrack.py
+++ src/hosttrack.py
@@ -20,8 +20,8 @@ print(time, "hosts up: ", len(hosts_list))
 for host, status in hosts_list:
     print(host + ' ' + status)
 
-fp = open("hackingdata.csv","a")
-#fp = open("hostmm.csv","a")        #for Malaviya
+#fp = open("hackingdata.csv","a")
+fp = open("hostmm.csv","a")        #for Malaviya
 w = csv.writer(fp)
 row = [time, len(hosts_list)]
 w.writerow(row)
